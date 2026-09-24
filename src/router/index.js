6f877c5be44464438dfe955c94f032b08fb70f2d
Ruddy <ruddy391@gmail.com>
2026-09-24 11:39:27 +0200

    refactor: reorganize frontend architecture by restructuring layout components and updating routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../components/Home.vue";
-import LoginView from "../Views/LoginView.vue";
-import RegisterView from "../Views/RegisterView.vue";
+import LoginView from "../Views/Auth/LoginView.vue";
+import RegisterView from "../Views/Auth/RegisterView.vue";
 import CartView from "../Views/CartView.vue";
 import MyAccountView from "../Views/MyAccountView.vue";
 import CustomerProfileView from "../Views/CustomerProfileView.vue";
-import AdminLayout from "../Views/Admin/AdminLayout.vue";
+import AdminLayout from "../layouts/AdminLayout.vue";
 import AdminDashboardView from "../Views/Admin/AdminDashboardView.vue";
 import AdminProductsView from "../Views/Admin/AdminProductsView.vue";
 import AdminOrdersView from "../Views/Admin/AdminOrdersView.vue";
@@ -18,20 +18,84 @@ import KitchenDashboardView from "../Views/KitchenDashboardView.vue";
 import SpecialOffersView from "../Views/SpecialOffersView.vue";
 import EventsCalendarView from "../Views/EventsCalendarView.vue";
 import PaymentView from "../Views/PaymentView.vue";
-import DeliveryLayout from "../Views/DeliveryLayout.vue";
+import DeliveryLayout from "../layouts/DeliveryLayout.vue";
 import DeliveryDashboardView from "../Views/DeliveryDashboardView.vue";
 import DeliveryOrdersView from "../Views/DeliveryOrdersView.vue";
+import PublicLayout from "../layouts/PublicLayout.vue";
 
 const routes = [
-  { path: "/", name: "home", component: Home, alias: "/home" },
-  { path: "/login", name: "login", component: LoginView, alias: "/login" },
-  { path: "/register", name: "register", component: RegisterView },
-  { path: "/cart", name: "cart", component: CartView, alias: "/cart" },
-  { path: "/account", name: "account", component: MyAccountView },
+
+{
+  path: "/",
+  component: PublicLayout,
+  children: [
+    {
+      path: "",
+      name: "home",
+      component: Home,
+      alias: "/home",
+    },
+    {
+      path: "login",
+      name: "login",
+      component: LoginView,
+    },
+    {
+      path: "register",
+      name: "register",
+      component: RegisterView,
+    },
+    {
+      path: "cart",
+      name: "cart",
+      component: CartView,
+    },
+    {
+      path: "account",
+      name: "account",
+      component: MyAccountView,
+    },
+    {
+      path: "account/profile",
+      name: "customer-profile",
+      component: CustomerProfileView,
+    },
+    {
+      path: "product/:id",
+      name: "product-detail",
+      component: ProductDetailView,
+    },
+    {
+      path: "carta",
+      name: "carta",
+      component: CartaView,
+    },
+    {
+      path: "ofertas-eventos",
+      name: "special-offers",
+      component: SpecialOffersView,
+    },
+    {
+      path: "calendario-eventos",
+      name: "events-calendar",
+      component: EventsCalendarView,
+    },
+    {
+      path: "payment",
+      name: "payment",
+      component: PaymentView,
+    },
+    {
+      path: "reservation",
+      name: "reservation",
+      component: ReservationView,
+    },
+  ],
+},  
   {
-    path: "/account/profile",
-    name: "customer-profile",
-    component: CustomerProfileView,
+    path: "/admin/welcome",
+    name: "admin-welcome",
+    component: AdminWelcomeView,
   },
   {
     path: "/admin",
@@ -43,7 +107,9 @@ const routes = [
         name: "admin-products",
         component: AdminProductsView,
       },
-      { path: "pedidos", name: "admin-orders", component: AdminOrdersView },
+      { path: "pedidos", 
+        name: "admin-orders", 
+        component: AdminOrdersView },
       {
         path: "facturacion",
         name: "admin-billing",
@@ -52,36 +118,10 @@ const routes = [
     ],
   },
   {
-    path: "/admin/welcome",
-    name: "admin-welcome",
-    component: AdminWelcomeView,
-  },
-  {
-    path: "/product/:id",
-    name: "product-detail",
-    component: ProductDetailView,
-  },
-  {
-    path: "/carta",
-    name: "carta",
-    component: CartaView,
-  },
-  {
     path: "/cocina",
     name: "kitchen-dashboard",
     component: KitchenDashboardView,
   },
-  {
-    path: "/ofertas-eventos",
-    name: "special-offers",
-    component: SpecialOffersView,
-  },
-  {
-    path: "/calendario-eventos",
-    name: "events-calendar",
-    component: EventsCalendarView,
-  },
-  { path: "/payment", name: "payment", component: PaymentView },
   {
     path: "/motorista",
     component: DeliveryLayout,
@@ -97,13 +137,7 @@ const routes = [
         component: DeliveryOrdersView,
       },
     ],
-  },
-  {
-    path: "/reservation",
-    name: "reservation",
-    component: ReservationView,
-    alias: "/reservation",
-  },
+  }
 ];
 
 export default createRouter({
